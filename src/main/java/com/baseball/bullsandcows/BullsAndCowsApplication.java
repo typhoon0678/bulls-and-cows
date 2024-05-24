2bff536a4a52c38ce10a09473079ec2695672abb
typhoon0678 <typhoon0678@gmail.com>
2024-05-24 20:12:42 +0900

    first commit

---
+++ src/main/java/com/baseball/bullsandcows/BullsAndCowsApplication.java
@@ -0,0 +1,13 @@
+package com.baseball.bullsandcows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BullsAndCowsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BullsAndCowsApplication.class, args);
+    }
+
+}
