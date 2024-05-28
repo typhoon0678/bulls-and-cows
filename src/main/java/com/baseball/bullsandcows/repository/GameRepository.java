2c204e45c12f903d08c4cb5dedfecf25eb84116c
Frod90 <142146516+Frod90@users.noreply.github.com>
2024-05-28 23:17:37 +0900

    Feat: Implement update game for next stage and find game by game ID and user ID function(in DB)

--- src/main/java/com/baseball/bullsandcows/repository/GameRepository.java
+++ src/main/java/com/baseball/bullsandcows/repository/GameRepository.java
@@ -1,8 +1,26 @@
 package com.baseball.bullsandcows.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.baseball.bullsandcows.domain.Game;
+import com.baseball.bullsandcows.domain.User;
 
+@Repository
 public interface GameRepository extends JpaRepository<Game, Long> {
+
+	Optional<Game> findByIdAndUser(Long id, User user);
+
+	@Modifying
+	@Query("update Game g set  g.nums = :nums, g.score = g.score + :score, g.tryCount = 5 where g.id = :id and g.user = :user")
+	int updateNextStageGame(
+		@Param("nums") String nums,
+		@Param("score") int score,
+		@Param("id") Long id,
+		@Param("user") User user);
 }
