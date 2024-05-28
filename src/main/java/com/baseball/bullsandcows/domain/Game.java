57387050e1becbcd6da446711ed0912bdf18585f
Frod90 <142146516+Frod90@users.noreply.github.com>
2024-05-28 23:03:13 +0900

    Feat: Implement validate baseball nums(userNums, gameNums) function

--- src/main/java/com/baseball/bullsandcows/domain/Game.java
+++ src/main/java/com/baseball/bullsandcows/domain/Game.java
@@ -1,6 +1,7 @@
 package com.baseball.bullsandcows.domain;
 
 import java.time.LocalDateTime;
+import java.util.regex.Pattern;
 
 import org.hibernate.annotations.ColumnDefault;
 
@@ -14,10 +15,12 @@ import jakarta.persistence.ManyToOne;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @Getter
 @NoArgsConstructor
+@ToString
 public class Game {
 
 	@Id
@@ -46,11 +49,41 @@ public class Game {
 
 	@Builder
 	public Game(User user, int score, int tryCount, String nums, boolean isFinished) {
+
+		validateNums(nums);
+
 		this.user = user;
 		this.score = score;
 		this.tryCount = tryCount;
 		this.nums = nums;
 		this.isFinished = isFinished;
 		this.playDate = LocalDateTime.now();
+	}
+
+	private void validateNums(String nums) {
+		validateNumsSize(nums);
+		validateNumsRange(nums);
+	}
+
+	private void validateNumsSize(String nums) {
+
+		if (nums.length() != 3) {
+			throw new IllegalStateException("[ERROR] 3자리 숫자만 가능합니다.");
+		}
+	}
+
+	private void validateNumsRange(String nums) {
+
+		Pattern pattern = Pattern.compile("^[1-9]");
+
+		String[] split = nums.split("");
+
+		for (String eachNum : split) {
+
+			if (!pattern.matcher(eachNum).matches()) {
+				throw new IllegalStateException("[ERROR] 1~9 범위의 숫자만 가능합니다.");
+			}
+		}
+
 	}
 }
