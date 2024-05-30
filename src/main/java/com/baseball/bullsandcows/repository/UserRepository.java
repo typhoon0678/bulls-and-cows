945918d057086cb7738d5fc6053707b4d177ff4f
ksngh <chzhqk98@naver.com>
2024-05-30 15:17:35 +0900

    feat : 회원가입 & 로그인 -> UserRepository에 findByEmail추가

--- src/main/java/com/baseball/bullsandcows/repository/UserRepository.java
+++ src/main/java/com/baseball/bullsandcows/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.baseball.bullsandcows.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,5 @@ import com.baseball.bullsandcows.domain.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+	Optional<User> findByEmail(String email);
 }
