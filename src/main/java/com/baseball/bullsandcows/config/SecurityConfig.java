ad369ca78b4110dd1ceb37f2889844a9474f9f7e
typhoon0678 <typhoon0678@gmail.com>
2024-06-06 17:46:40 +0900

    Fix: 중복된 매핑 경로 수정 임시 메서드 /api/checkNum 제거

--- src/main/java/com/baseball/bullsandcows/config/SecurityConfig.java
+++ src/main/java/com/baseball/bullsandcows/config/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
 					.permitAll()
 					.anyRequest()
 					.permitAll())
-			// .authenticated())
+					// .authenticated())
 			.oauth2Login(oauth2Login -> oauth2Login.userInfoEndpoint(
 				userInfoEndpointConfig -> userInfoEndpointConfig.userService(customOAuth2UserService)));
 
--- src/main/java/com/baseball/bullsandcows/controller/GameApiController.java
+++ src/main/java/com/baseball/bullsandcows/controller/GameApiController.java
@@ -1,34 +1,75 @@
 package com.baseball.bullsandcows.controller;
 
+import java.nio.charset.StandardCharsets;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.baseball.bullsandcows.domain.GameDTO;
+import com.baseball.bullsandcows.domain.GameForm;
+import com.baseball.bullsandcows.domain.User;
 import com.baseball.bullsandcows.dto.CheckNumRequest;
 import com.baseball.bullsandcows.dto.CheckNumResponse;
 import com.baseball.bullsandcows.service.GameService;
+import com.baseball.bullsandcows.service.UserService;
 
-import lombok.RequiredArgsConstructor;
-
-@RequiredArgsConstructor
 @RestController
-@RequestMapping(value = "/api")
 public class GameApiController {
 
-	private final GameService gameService;
+	GameService gameService;
+	UserService userService;
 
-	@PostMapping("/checkNum")
-	public CheckNumResponse checkNum(@RequestBody CheckNumRequest checkNumRequest) {
+	@Autowired
+	public GameApiController(GameService gameService, UserService userService) {
+		this.gameService = gameService;
+		this.userService = userService;
+	}
 
-		return CheckNumResponse.builder()
-			.gameID(checkNumRequest.getGameID())
-			.userID(checkNumRequest.getUserID())
-			.addScore(100)
-			.totalScore(1000)
-			.tryCount(4)
-			.gameResult("next")  // "continue", "next", "gameOver"
-			.judgement(new int[] {1, 1})  // "strike", "ball", ("out")
-			.build();
+	@GetMapping("/api/checkNum")
+	public String findRound() {
+
+		return "/index";
+	}
+
+	@PostMapping("/api/checkNum")
+	public ResponseEntity<GameDTO> conductRound(@RequestBody GameForm gameForm) {
+
+		try {
+
+			User user = userService.findById(gameForm.getUserID());
+
+			if (user == null) {
+				throw new IllegalArgumentException("[ERROR] 존재하지 않는 유저입니다.");
+			}
+			HttpHeaders headers = new HttpHeaders();
+			headers.setContentType(new MediaType("application", "json", StandardCharsets.UTF_8));
+
+			switch (gameForm.getGameOption()) {
+				case "new" -> {
+					return new ResponseEntity<>(gameService.newGame(user), headers, HttpStatus.OK);
+				}
+				case "next" -> {
+					return new ResponseEntity<>(gameService.moveNextStage(user, gameForm.getGameID()), headers,
+						HttpStatus.OK);
+				}
+				case "continue" -> {
+					return new ResponseEntity<>(gameService.judgeGame(gameForm.getNum(), gameForm.getGameID(), user),
+						headers, HttpStatus.OK);
+				}
+				default -> {
+					return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+				}
+			}
+
+		} catch (IllegalStateException | IllegalArgumentException e) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
 	}
 }
